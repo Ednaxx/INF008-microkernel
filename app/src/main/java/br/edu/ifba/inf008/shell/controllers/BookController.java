2cc1601a6c9909d18a4cc95521fbb2482281a070
Alexandre Morais <xandre2setembro2002@gmail.com>
2025-02-13 19:29:25 -0300

    feat: implemented book and user controllers

--- app/src/main/java/br/edu/ifba/inf008/shell/controllers/BookController.java
+++ app/src/main/java/br/edu/ifba/inf008/shell/controllers/BookController.java
@@ -19,20 +19,36 @@ public class BookController {
         return bookController;
     }
 
+
     public void addBook(BookModel book) {
         books.add(book);
     }
 
-    public List<BookModel> getBooks() {
+    public List<BookModel> getAll() {
         return books;
     }
 
-    public BookModel findBookByIsbn(String isbn) {
+    public BookModel getByISBN(String isbn) {
         for (BookModel book : books) {
             if (book.getIsbn().equals(isbn)) {
                 return book;
             }
         }
         return null;
+    }
+
+    public void updateBook(String isbn, BookModel updatedBook) {
+        for (BookModel book : books) {
+            if (book.getIsbn().equals(isbn)) {
+                book.setTitle(updatedBook.getTitle());
+                book.setAuthor(updatedBook.getAuthor());
+                book.setGenre(updatedBook.getGenre());
+                book.setReleaseDate(updatedBook.getReleaseDate());
+            }
+        }
+    }
+
+    public void deleteBook(String isbn) {
+        books.removeIf(book -> book.getIsbn().equals(isbn));
     }
 }
--- app/src/main/java/br/edu/ifba/inf008/shell/controllers/UserController.java
+++ app/src/main/java/br/edu/ifba/inf008/shell/controllers/UserController.java
@@ -1,9 +1,11 @@
 package br.edu.ifba.inf008.shell.controllers;
 
+import br.edu.ifba.inf008.shell.models.BookModel;
 import br.edu.ifba.inf008.shell.models.UserModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class UserController {
     private static UserController userController;
@@ -19,21 +21,61 @@ public class UserController {
         return userController;
     }
 
-
     public void addUser(UserModel user) {
         users.add(user);
     }
 
-    public List<UserModel> getUsers() {
+    public List<UserModel> getAll() {
         return users;
     }
 
-    public UserModel findUserByUsername(String username) {
+    public UserModel getById(UUID id) {
         for (UserModel user : users) {
-            if (user.getUsername().equals(username)) {
+            if (user.getId().equals(id)) {
                 return user;
             }
         }
         return null;
+    }
+
+    public UserModel getByEmail(String email) {
+        for (UserModel user : users) {
+            if (user.getEmail().equals(email)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void updateUser(UUID id, UserModel updatedUser) {
+        for (UserModel user : users) {
+            if (user.getId().equals(id)) {
+                user.setFirstName(updatedUser.getFirstName());
+                user.setLastName(updatedUser.getLastName());
+                user.setEmail(updatedUser.getEmail());
+            }
+        }
+    }
+
+    public void deleteUser(UUID id) {
+        users.removeIf(user -> user.getId().equals(id));
+    }
+
+    public void borrowBook(UUID userId, String isbn) {
+        UserModel user = getById(userId);
+        BookModel book = BookController.getInstance().getByISBN(isbn);
+
+        if (user != null && book != null) {
+            user.getBorrowedBooks().add(book);
+        }
+    }
+
+    public void returnBook(UUID userId, String isbn) {
+        UserModel user = getById(userId);
+        BookModel book = BookController.getInstance().getByISBN(isbn);
+
+        if (user != null && book != null) {
+            user.getBorrowedBooks().remove(book);
+        }
     }
 }
